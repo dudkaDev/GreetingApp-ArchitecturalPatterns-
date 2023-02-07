86f9e9973519331624afade8b425230d418977de
Andrey Abakumov <andrey9864rus@mail.ru>
2023-02-07 15:01:42 +0400

    шаблон

---
+++ GreetingApp/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  GreetingApp
+//
+//  Created by Андрей Абакумов on 07.02.2023.
+//
+
+import Foundation
--- GreetingApp/ViewController.swift
+++ GreetingApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+    }
 }
 
